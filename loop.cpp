a6ba023071956934b92dd214f06d2dbb4fac9328
Mahnoor-Sajjadgit config --global user.email mahnoorsajjad.se@gmail.com <mahnoorsajjad.se@gmail.com>
2024-07-24 21:53:17 +0500

    Added initial files

---
+++ loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout <<"Practicing git hub";
+    for (int  i = 0; i < 4; i++)
+    {
+        cout << "Hello world\n";
+    }
+    
+    return 0;
+}
---
+++ simple.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"This is a practice program";
+    return 0;
+}
